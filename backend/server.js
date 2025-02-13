65b6dc6b6b56a4aa80c5db2b2517c2360b59182d
SanDace <sandeshmaharjan88@gmail.com>
2025-02-13 20:15:24 +0000

    4th commit

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,8 @@ const connectDB = async () => {
         if (!process.env.MONGODB_URI) {
             throw new Error('MongoDB URI is not defined in environment variables');
         }
-
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected...');
     } catch (error) {
         console.error('MongoDB connection error:', error);
